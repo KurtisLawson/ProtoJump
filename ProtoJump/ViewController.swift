0fbd6dc5c1b6005c487c6914737c624f8a6f47af
Henry Zhang <a01046805@se12l0325-20.ad.bcit.ca>
2021-02-27 15:56:52 -0800

    Imported Box2D and modified settings

--- ProtoJump/ViewController.swift
+++ ProtoJump/ViewController.swift
@@ -1,19 +1,47 @@
 //
-//  ViewController.swift
-//  ProtoJump
-//
-//  Created by Kurtis Lawson on 2021-02-18.
+//  Copyright © Borna Noureddin. All rights reserved.
 //
 
-import UIKit
+import GLKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+extension ViewController: GLKViewControllerDelegate {
+    func glkViewControllerUpdate(_ controller: GLKViewController) {
+        glesRenderer.update()
     }
-
-
 }
 
+class ViewController: GLKViewController {
+    
+    
+    private var context: EAGLContext?
+    private var glesRenderer: Renderer!
+    
+    private func setupGL() {
+        context = EAGLContext(api: .openGLES3)
+        EAGLContext.setCurrent(context)
+        if let view = self.view as? GLKView, let context = context {
+            view.context = context
+            delegate = self as GLKViewControllerDelegate
+            glesRenderer = Renderer()
+            glesRenderer.setup(view)
+            glesRenderer.loadModels()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupGL()
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(self.doSingleTap(_:)))
+        singleTap.numberOfTapsRequired = 1
+        view.addGestureRecognizer(singleTap)
+    }
+    
+    override func glkView(_ view: GLKView, drawIn rect: CGRect) {
+        glesRenderer.draw(rect)
+    }
+    
+    @objc func doSingleTap(_ sender: UITapGestureRecognizer) {
+        glesRenderer.box2d.launchBall()
+    }
+
+}
