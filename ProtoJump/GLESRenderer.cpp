874458ca19c0aec4d859a88a2d04b936cb9e33fe
Kurtis Lawson <kurtislawson@live.com>
2021-04-06 16:27:09 -0700

    Added gensphere function to replace existing player rendiering

--- ProtoJump/GLESRenderer.cpp
+++ ProtoJump/GLESRenderer.cpp
@@ -299,3 +299,89 @@ int GLESRenderer::GenCube(float scale, float **vertices, float **normals,
     
     return numIndices;
 }
+
+int GLESRenderer::GenSphere(int numSlices, float radius, float **vertices,
+                            float **normals, float **texCoords,
+                            int **indices)
+{
+    int i;
+    int j;
+    int numParallels = numSlices / 2;
+    int numVertices = ( numParallels + 1 ) * ( numSlices + 1 );
+    int numIndices = numParallels * numSlices * 6;
+    float angleStep = ( 2.0f * M_PI ) / ( ( float ) numSlices );
+    
+    // Allocate memory for buffers
+    if ( vertices != NULL )
+    {
+        *vertices = (GLfloat *)malloc ( sizeof ( GLfloat ) * 3 * numVertices );
+    }
+    
+    if ( normals != NULL )
+    {
+        *normals = (GLfloat *)malloc ( sizeof ( GLfloat ) * 3 * numVertices );
+    }
+    
+    if ( texCoords != NULL )
+    {
+        *texCoords = (GLfloat *)malloc ( sizeof ( GLfloat ) * 2 * numVertices );
+    }
+    
+    if ( indices != NULL )
+    {
+        *indices = (int *)malloc ( sizeof ( int ) * numIndices );
+    }
+    
+    for ( i = 0; i < numParallels + 1; i++ )
+    {
+        for ( j = 0; j < numSlices + 1; j++ )
+        {
+            int vertex = ( i * ( numSlices + 1 ) + j ) * 3;
+            
+            if ( vertices )
+            {
+                ( *vertices ) [vertex + 0] = radius * sinf ( angleStep * ( float ) i ) *
+                sinf ( angleStep * ( float ) j );
+                ( *vertices ) [vertex + 1] = radius * cosf ( angleStep * ( float ) i );
+                ( *vertices ) [vertex + 2] = radius * sinf ( angleStep * ( float ) i ) *
+                cosf ( angleStep * ( float ) j );
+            }
+            
+            if ( normals )
+            {
+                ( *normals ) [vertex + 0] = ( *vertices ) [vertex + 0] / radius;
+                ( *normals ) [vertex + 1] = ( *vertices ) [vertex + 1] / radius;
+                ( *normals ) [vertex + 2] = ( *vertices ) [vertex + 2] / radius;
+            }
+            
+            if ( texCoords )
+            {
+                int texIndex = ( i * ( numSlices + 1 ) + j ) * 2;
+                ( *texCoords ) [texIndex + 0] = ( float ) j / ( float ) numSlices;
+                ( *texCoords ) [texIndex + 1] = ( 1.0f - ( float ) i ) / ( float ) ( numParallels - 1 );
+            }
+        }
+    }
+    
+    // Generate the indices
+    if ( indices != NULL )
+    {
+        int *indexBuf = ( *indices );
+        
+        for ( i = 0; i < numParallels ; i++ )
+        {
+            for ( j = 0; j < numSlices; j++ )
+            {
+                *indexBuf++  = i * ( numSlices + 1 ) + j;
+                *indexBuf++ = ( i + 1 ) * ( numSlices + 1 ) + j;
+                *indexBuf++ = ( i + 1 ) * ( numSlices + 1 ) + ( j + 1 );
+                
+                *indexBuf++ = i * ( numSlices + 1 ) + j;
+                *indexBuf++ = ( i + 1 ) * ( numSlices + 1 ) + ( j + 1 );
+                *indexBuf++ = i * ( numSlices + 1 ) + ( j + 1 );
+            }
+        }
+    }
+    
+    return numIndices;
+}
