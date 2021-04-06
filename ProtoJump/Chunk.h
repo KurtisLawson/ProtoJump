bae4a110aff8ba2c1f77614cc170234a5d14d234
Houman <hgholami@my.bcit.ca>
2021-04-06 10:03:13 -0700

    Chunk files created

---
+++ ProtoJump/Chunk.h
@@ -0,0 +1,14 @@
+//
+//  Chunk.h
+//  ProtoJump
+//
+//  Created by socas on 2021-04-06.
+//
+
+#ifndef Chunk_h
+#define Chunk_h
+
+#import <Foundation/Foundation.h>
+
+
+#endif /* Chunk_h */
---
+++ ProtoJump/Chunk.mm
@@ -0,0 +1,8 @@
+//
+//  Chunk.m
+//  ProtoJump
+//
+//  Created by Houman on 2021-04-04.
+//
+
+#include "Chunk.h"
